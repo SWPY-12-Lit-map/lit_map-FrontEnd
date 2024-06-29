de72ceaf518ed6ff5474b26245123fab39faf78d
gyozaa01 <olivia110321@gmail.com>
2024-06-29 23:17:07 +0900

    feat: 회원가입 페이지

--- Litmap/src/App.jsx
+++ Litmap/src/App.jsx
@@ -1,9 +1,11 @@
 import Connection from "./Pages/Connection";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <>
       <Connection></Connection>
+      <Signup />
     </>
   );
 }
