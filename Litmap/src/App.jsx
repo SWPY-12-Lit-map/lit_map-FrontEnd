b8aa5bddb8ce85b3499cd9445b18fcdc333a318c
strdeok <ejraks1548@gmail.com>
2024-07-02 01:54:02 +0900

    Merge branch 'develop' of https://github.com/SWPY-12-Lit-map/lit_map-FrontEnd into develop

--- Litmap/src/App.jsx
+++ Litmap/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Navbar";
-import Connection from "./Pages/Connection";
+import Post from "./Pages/Post";
 import Signup from "./Pages/Signup";
 import Home from "./Pages/Home";
 
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/category1" element={<Connection />} />
+        <Route path="/category1" element={<Post />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
