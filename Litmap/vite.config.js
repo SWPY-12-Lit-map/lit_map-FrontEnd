ce946655622953e628f8137f25d97e5ec37ae248
gyozaa01 <olivia110321@gmail.com>
2024-07-20 20:50:58 +0900

    feat: 회원가입 2단계까지 연동

--- Litmap/vite.config.js
+++ Litmap/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.litmap.store',
+        changeOrigin: true,
+        secure: false, // 필요한 경우 SSL 인증서가 없는 백엔드 서버를 처리하기 위해 사용
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
