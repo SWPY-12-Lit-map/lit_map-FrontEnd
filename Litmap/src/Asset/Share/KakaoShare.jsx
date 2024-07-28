40a4b3de20dbc530d6729a792a404fcaaea9256d
strdeok <ejraks1548@gmail.com>
2024-07-28 13:03:20 +0900

    fix: 카카오 공유하기 템플릿 수정

--- Litmap/src/Asset/Share/KakaoShare.jsx
+++ Litmap/src/Asset/Share/KakaoShare.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import styled from "styled-components";
+import { useStore } from "../store";
 // kakao 기능 동작을 위해 넣어준다.
 const { Kakao } = window;
+import thumbnailImg from "/thumbnail.png";
+import { useParams } from "react-router-dom";
 
 const ShareBtn = styled.button`
   background-color: #8d2741;
@@ -12,11 +15,11 @@ const ShareBtn = styled.button`
   border-radius: 50%;
 `;
 
-export default function KakaoShare(props) {
+export default function KakaoShare() {
   // 배포한 자신의 사이트
-  const realUrl = "http://localhost:5173/";
+
+  const realUrl = window.location.href;
   // 로컬 주소 (localhost 3000 같은거)
-  const imgUrl = props.imgUrl;
 
   useEffect(() => {
     Kakao.cleanup();
@@ -28,10 +31,14 @@ export default function KakaoShare(props) {
     Kakao.Share.sendDefault({
       objectType: "feed",
       content: {
-        title: "인물관계도 확인해보기",
-        description: "인물 관계도 확인해보세요",
-        imageUrl: "", // 이미지는  800 x 400이 적당
+        title: "인물관계도를 확인해보기",
+        description: "릿맵에서 인물관계도를 확인해보세요",
+        imageUrl:
+          " https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihaTcLucEeuEHMOv1WsTKoocrKsCSwhF4tWPg2xu4XrfBI0UyJ98Q3HtBcPRm3SX4RX460CvOaRzGP4Q55g3Xaq9Uvs3ex4Fvpo=w3160-h2048-rw-v1", // 이미지는  800 x 400이 적당
+        imageWidth: 800,
+        imageHeight: 400,
         link: {
+          mobileWebUrl: realUrl,
           webUrl: realUrl,
         },
       },
@@ -40,6 +47,7 @@ export default function KakaoShare(props) {
           title: "사이트로 이동하기",
           link: {
             mobileWebUrl: realUrl,
+            webUrl: realUrl,
           },
         },
       ],
