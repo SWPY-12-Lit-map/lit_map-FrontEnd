4eb61e0ce69f687b3c64a3b96b708b739588e611
strdeok <ejraks1548@gmail.com>
2024-08-03 15:40:44 +0900

    feat: svg 파일

--- Litmap/vite.config.js
+++ Litmap/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://api.litmap.store',
+      "/api": {
+        target: "https://api.litmap.store",
         changeOrigin: true,
         secure: false, // 필요한 경우 SSL 인증서가 없는 백엔드 서버를 처리하기 위해 사용
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
